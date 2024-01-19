0d05d2263b9056dbeeb4f42b707aeb74fbd89480
wwsheng009 <vincentwwsheng@gmail.com>
2024-01-20 01:09:16 +0800

    build yao builder docker

---
+++ docker/build-custom/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /app && \
+git clone https://github.com/wwsheng009/kun.git /app/kun && \
+git clone https://github.com/wwsheng009/xun.git /app/xun && \
+git clone https://github.com/wwsheng009/gou.git /app/gou && \
+git clone https://github.com/wwsheng009/v8go.git /app/v8go && \
+git clone https://github.com/wwsheng009/xgen.git /app/xgen-v1.0 && \
+git clone https://github.com/wwsheng009/yao-init.git /app/yao-init && \
+git clone https://github.com/wwsheng009/yao.git /app/yao
+
+
+cd /app/yao && \
+export VERSION=$(cat share/const.go  |grep 'const VERSION' | awk '{print $4}' | sed "s/\"//g") 
+
+cd /app/yao && make tools && make artifacts-linux
+mv /app/yao/dist/release/* /data/
