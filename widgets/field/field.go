6b45903382a11d1b5e97ef291c8070c62151833f
wwsheng009 <vincentwwsheng@gmail.com>
2024-03-19 15:51:47 +0800

    adapt diffferent db driver

--- widgets/field/field.go
+++ widgets/field/field.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/yaoapp/yao/config"
 	"github.com/yaoapp/yao/data"
@@ -12,9 +13,13 @@ import (
 
 // LoadAndExport load table
 func LoadAndExport(cfg config.Config) error {
-
+	transFilename := "model.trans.json"
+	// use different transform for not mysql db
+	if strings.ToLower(os.Getenv("YAO_DB_DRIVER")) != "mysql" {
+		transFilename = "model-not-mysql.trans.json"
+	}
 	if os.Getenv("YAO_DEV") != "" {
-		file := filepath.Join(os.Getenv("YAO_DEV"), "yao", "fields", "model.trans.json")
+		file := filepath.Join(os.Getenv("YAO_DEV"), "yao", "fields", transFilename)
 		source, err := ioutil.ReadFile(file)
 		if err != nil {
 			return err
@@ -25,7 +30,7 @@ func LoadAndExport(cfg config.Config) error {
 		}
 	}
 
-	source, err := data.Read(filepath.Join("yao", "fields", "model.trans.json"))
+	source, err := data.Read(filepath.Join("yao", "fields", transFilename))
 	if err != nil {
 		return err
 	}
