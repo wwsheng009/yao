14947b849e0290f3e337ba22e1f54d66fe876813
wwsheng009 <vincentwwsheng@gmail.com>
2025-03-28 23:24:18 +0800

    feat(api): 在streamChat中添加输出类型和属性
    在streamChat函数中，扩展了输出消息的映射，增加了类型和属性字段，以便更全面地传递响应信息

--- neo/assistant/api.go
+++ neo/assistant/api.go
@@ -593,7 +593,10 @@ func (ast *Assistant) streamChat(
 				// The default output
 				output := chatMessage.New().Done()
 				if res != nil && res.Output != nil && len(res.Output) > 0 {
-					output = chatMessage.New().Map(map[string]interface{}{"text": string(res.Output[0].Bytes), "done": true})
+					output = chatMessage.New().Map(map[string]interface{}{"text": string(res.Output[0].Bytes),
+						"type":  res.Output[0].Type,
+						"props": res.Output[0].Props,
+						"done":  true})
 					output.Retry = ctx.Retry
 					output.Silent = ctx.Silent
 				}
