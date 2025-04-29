9c572703cb8f5d4551bbebd09e16b96e25edf499
wwsheng009 <vincentwwsheng@gmail.com>
2025-04-29 16:17:26 +0800

    chore: 删除 build_init.ps1 文件
    该脚本用于初始化项目时克隆并清理 yao-init 仓库，现已不再需要，因此删除此文件以简化项目结构。

--- build_init.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item .tmp\yao-init -Recurse -Force
-git clone --depth=1 https://github.com/wwsheng009/yao-init.git .tmp\yao-init
-Remove-Item .tmp\yao-init\.git -Recurse -Force
-Remove-Item .tmp\yao-init\.gitignore
-Remove-Item .tmp\yao-init\LICENSE
-Remove-Item .tmp\yao-init\README.md
